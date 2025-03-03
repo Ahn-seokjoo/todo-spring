70ccbe707f8f2c2554cfc1ee182db610ba24a1b6
Ahn-seokjoo <tjrwn0716@naver.com>
2025-03-03 17:23:59 +0900

    [feature] 카테고리 삭제시, 참조하고 있는 개수가 0개면 카테고리도 같이 삭제

--- src/main/kotlin/com/seokjoo/todo/domain/repository/CategoryRepository.kt
+++ src/main/kotlin/com/seokjoo/todo/domain/repository/CategoryRepository.kt
@@ -2,7 +2,12 @@ package com.seokjoo.todo.domain.repository
 
 import com.seokjoo.todo.domain.entity.Category
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
-interface CategoryRepository : JpaRepository<Category, Long>
+interface CategoryRepository : JpaRepository<Category, Long> {
+
+    @Query("select count(tc) from TodoCategories tc.category.id where tc.category.id = :categoryId")
+    fun countByCategoryId(categoryId: Long): Long
+}
--- src/main/kotlin/com/seokjoo/todo/presentation/category/service/CategoryService.kt
+++ src/main/kotlin/com/seokjoo/todo/presentation/category/service/CategoryService.kt
@@ -2,8 +2,18 @@ package com.seokjoo.todo.presentation.category.service
 
 import com.seokjoo.todo.domain.repository.CategoryRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class CategoryService(
-    private val repository: CategoryRepository,
-)
+    private val categoryRepository: CategoryRepository,
+) {
+
+    @Transactional
+    fun removeCategory(categoryId: Long) {
+        val count = categoryRepository.countByCategoryId(categoryId)
+        if (count == 0L) {
+            categoryRepository.deleteById(categoryId)
+        }
+    }
+}
