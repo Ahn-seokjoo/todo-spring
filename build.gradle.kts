8e0cf2342775b7fbb2c230a1ad708e960577aaec
Ahn-seokjoo <tjrwn0716@naver.com>
2025-03-09 04:31:44 +0900

    [#7] todo service 테스트 코드 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,11 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testImplementation("io.mockk:mockk:1.13.17")
+	// kotest
+	testImplementation("io.kotest:kotest-runner-junit5:5.4.2")
+	testImplementation("io.kotest:kotest-assertions-core:5.4.2")
+	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
 }
 
 kotlin {
