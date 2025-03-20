c992b2f2847cbf75051269f303355de5bf69fd2e
Ahn-seokjoo <tjrwn0716@naver.com>
2025-03-21 01:18:15 +0900

    [#7] todo 통합 테스트 완성

--- src/main/kotlin/com/seokjoo/todo/domain/service/todo/TodoService.kt
+++ src/main/kotlin/com/seokjoo/todo/domain/service/todo/TodoService.kt
@@ -57,25 +57,29 @@ class TodoService(
     @Transactional
     fun deleteTodo(id: Long) {
         val todo = todoRepository.findByIdOrNull(id) ?: throw TodoException.of(TodoExceptionType.NOT_EXISTED_TODO)
-        val todoCategory = todo.todoCategories
-        todoRepository.deleteById(id)
 
         /**
         1. 아래 로직은, 더이상 참조되지 않은 Category를 지워주기 위함
         2. 삭제한 투두에서 사용중이던 카테고리 목록들을 돌면서,
         3. null 아니고,
         4. 더이상 링킹 되지않은걸 지운다.
+        5. todo 를 제거하기 전에 category 먼저 제거한다
          */
 
-        todoCategory
+        todo.todoCategories
             .filter { it.category != null && isCategoryNotUsed(it) }
             .mapNotNull { it.category }
             .forEach { categoryRepository.delete(it) }
+
+        todoRepository.deleteById(id)
     }
 
+    /**
+     * 조회 이후에 delete 하기 때문에 count 1로 변경 (자기 자신)
+     */
     private fun isCategoryNotUsed(it: TodoCategory): Boolean {
         return it.category?.let { category ->
-            todoCategoryRepository.countByCategoryId(categoryId = category.id) == 0
+            todoCategoryRepository.countByCategoryId(categoryId = category.id) == 1
         } ?: false
     }
 
--- src/test/kotlin/com/seokjoo/todo/domain/service/todo/TodoServiceSpringBootTest.kt
+++ src/test/kotlin/com/seokjoo/todo/domain/service/todo/TodoServiceSpringBootTest.kt
@@ -4,9 +4,12 @@ import com.seokjoo.todo.domain.repository.category.CategoryRepository
 import com.seokjoo.todo.domain.repository.categorytodo.TodoCategoryRepository
 import com.seokjoo.todo.domain.repository.todo.TodoRepository
 import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.transaction.annotation.Transactional
 
@@ -15,10 +18,23 @@ import org.springframework.transaction.annotation.Transactional
 @ActiveProfiles("test")
 class TodoServiceSpringBootTest @Autowired constructor(
     private val service: TodoService,
-    private val repository: TodoRepository,
+    private val todoRepository: TodoRepository,
     private val categoryRepository: CategoryRepository,
     private val todoCategoryRepository: TodoCategoryRepository,
 ) {
+    var result: TodoServiceResponseDTO? = null
+
+    @BeforeEach
+    fun beforeEach() {
+        val request = TodoServiceRequestDTO(todo = "android", isDone = true, categoryNames = listOf("drama"))
+        // when
+        result = service.createTodo(request)
+    }
+
+    @AfterEach
+    fun afterEach() {
+        result = null
+    }
 
     // 통합 테스트
     @Test
@@ -29,7 +45,8 @@ class TodoServiceSpringBootTest @Autowired constructor(
         val result = service.createTodo(request)
 
         // then
-        Assertions.assertThat(result).usingRecursiveComparison()
+        Assertions.assertThat(result)
+            .usingRecursiveComparison()
             .ignoringFields("id")
             .isEqualTo(
                 TodoServiceResponseDTO(
@@ -39,5 +56,117 @@ class TodoServiceSpringBootTest @Autowired constructor(
                     id = 0L
                 )
             )
+    }
+
+    @Test
+    fun `getTodoById 테스트`() {
+        val id = result?.id ?: return
+
+        val todo = service.getTodoById(id)
+
+        Assertions.assertThat(todo)
+            .usingRecursiveComparison()
+            .isEqualTo(
+                TodoServiceResponseDTO(
+                    id = id,
+                    todo = "android",
+                    isDone = true,
+                    categories = listOf("drama"),
+                )
+            )
+    }
+
+    @Test
+    fun `getAllTodos 테스트`() {
+        /**
+         * 이미 한개가 존재하기 때문에, 한개 더 추가 이후 총 2개인지 테스트
+         * given
+         */
+        val request = TodoServiceRequestDTO("node")
+        service.createTodo(request)
+
+        // when
+        val todoList = service.getAllTodos()
+        // then
+        assert(todoList.size == 2)
+        Assertions.assertThat(todoList)
+            .usingRecursiveComparison()
+            .ignoringFields("id")
+            .isEqualTo(
+                listOf(
+                    TodoServiceResponseDTO(id = 1L, isDone = true, todo = "android", categories = listOf("drama")),
+                    TodoServiceResponseDTO(id = 2L, isDone = false, todo = "node", categories = listOf()),
+                )
+            )
+    }
+
+    @Test
+    fun `updateTodo 테스트`() {
+        // given
+        val updateId = result?.id ?: return
+        val request = TodoServiceRequestDTO(todo = "iOS", isDone = false, categoryNames = listOf("horror"))
+
+        // when
+        val result = service.updateTodo(id = updateId, request = request)
+
+        // then
+        Assertions.assertThat(result)
+            .isEqualTo(
+                TodoServiceResponseDTO(
+                    id = updateId,
+                    isDone = false,
+                    todo = "iOS",
+                    categories = listOf("drama", "horror"),
+                ),
+            )
+    }
+
+    /**
+     * getAll 같은 함수들은 영속성 컨텍스트에 남아있어서 제대로 테스트가 안됨 ,,
+     */
+    @Test
+    fun `delete 테스트 - 제거 이후 Todo DB 조회`() {
+        // given
+        val deleteId = result?.id ?: throw IllegalStateException("non-null is null")
+        service.deleteTodo(deleteId)
+
+        // when
+        val deletedEntity = todoRepository.findByIdOrNull(deleteId)
+
+        // then
+        assert(deletedEntity == null)
+    }
+
+    @Test
+    fun `delete 테스트 - 제거 이후 Category DB 조회`() {
+        // given
+        val beforeEntity = categoryRepository.findCategoryByName("drama")
+
+        val deleteId = result?.id ?: throw IllegalStateException("non-null is null")
+        service.deleteTodo(deleteId)
+
+        // when
+        val deletedEntity = categoryRepository.findCategoryByName("drama")
+
+        // then
+        assert(beforeEntity?.name == "drama")
+        assert(deletedEntity == null)
+    }
+
+    @Test
+    fun `delete 테스트 - 제거 이후 TodoCategory DB 조회, Cascade로 같이 잘 제거가 됐는지`() {
+        // given
+        val categoryId =
+            categoryRepository.findCategoryByName("drama")?.id ?: throw IllegalStateException("non-null is null")
+        val todoCategoryBeforeCountByRemove = todoCategoryRepository.countByCategoryId(categoryId)
+        val deleteId = result?.id ?: throw IllegalStateException("non-null is null")
+        service.deleteTodo(deleteId)
+
+        // when
+        val todoCategoryAfterCountByRemove = todoCategoryRepository.countByCategoryId(categoryId)
+
+        // then
+        assert(todoCategoryBeforeCountByRemove == 1)
+        assert(todoCategoryAfterCountByRemove == 0)
     }
 }
