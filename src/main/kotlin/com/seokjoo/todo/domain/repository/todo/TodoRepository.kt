ad3c053bfa58a61229b9eb2f85a3e3e0195500f4
Ahn-seokjoo <tjrwn0716@naver.com>
2025-03-22 21:29:11 +0900

    [#18] fetch join 사용?

--- src/main/kotlin/com/seokjoo/todo/domain/repository/todo/TodoRepository.kt
+++ src/main/kotlin/com/seokjoo/todo/domain/repository/todo/TodoRepository.kt
@@ -4,9 +4,11 @@ import com.seokjoo.todo.domain.entity.todo.Todo
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface TodoRepository : JpaRepository<Todo, Long> {
+    @Query("select t from Todo t left join fetch t.todoCategories tc left join fetch tc.category ORDER BY t.createdAt ASC")
     fun findAllByOrderByCreatedAtAsc(pageable: Pageable): Page<Todo>
 }
