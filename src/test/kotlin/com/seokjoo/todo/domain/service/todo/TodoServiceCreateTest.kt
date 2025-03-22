ab504b9eaccd8261567b395f8f670dea9d41b075
Ahn-seokjoo <tjrwn0716@naver.com>
2025-03-22 22:30:21 +0900

    [#18] 테스트 전체 다 주석처리

--- src/test/kotlin/com/seokjoo/todo/domain/service/todo/TodoServiceCreateTest.kt
+++ src/test/kotlin/com/seokjoo/todo/domain/service/todo/TodoServiceCreateTest.kt
@@ -1,25 +1,26 @@
-package com.seokjoo.todo.domain.service.todo
-
-import com.seokjoo.todo.domain.repository.todo.TodoRepository
-import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
-
-@SpringBootTest
-@ActiveProfiles("prod")
-class TodoServiceCreateTest @Autowired constructor(
-    private val todoRepository: TodoRepository,
-) {
-
-    /**
-     *  todo를 200개 생성해줍니다
-     *  테스트 용이니 내부는 주석처리 되어 있습니다. 사용 후에 주석 꼭 해주세용
-     */
-    @Test
-    fun `createTodo 200개 생성`() {
-        // for (i in 1..200) {
-        //     todoRepository.save(Todo(todo = "todo count : $i"))
-        // }
-    }
-}
+// package com.seokjoo.todo.domain.service.todo
+//
+// import com.seokjoo.todo.domain.entity.todo.Todo
+// import com.seokjoo.todo.domain.repository.todo.TodoRepository
+// import org.junit.jupiter.api.Test
+// import org.springframework.beans.factory.annotation.Autowired
+// import org.springframework.boot.test.context.SpringBootTest
+// import org.springframework.test.context.ActiveProfiles
+//
+// @SpringBootTest
+// @ActiveProfiles("prod")
+// class TodoServiceCreateTest @Autowired constructor(
+//     private val todoRepository: TodoRepository,
+// ) {
+//
+//     /**
+//      *  todo를 200개 생성해줍니다
+//      *  테스트 용이니 내부는 주석처리 되어 있습니다. 사용 후에 주석 꼭 해주세용
+//      */
+//     @Test
+//     fun `createTodo 200개 생성`() {
+//         for (i in 1..200) {
+//             todoRepository.save(Todo(todo = "todo count : $i"))
+//         }
+//     }
+// }
