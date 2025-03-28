3a782c9f03ba1ee859754bbfbb692e4c6e49e5aa
Ahn-seokjoo <tjrwn0716@naver.com>
2025-03-28 17:19:21 +0900

    [#16] jwt 생성 추가

---
+++ src/main/kotlin/com/seokjoo/todo/common/common/jwt/JwtProvider.kt
@@ -0,0 +1,50 @@
+package com.seokjoo.todo.common.common.jwt
+
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.util.Base64
+import java.util.Date
+
+@Component
+class JwtProvider(
+    @Value("\${jwt.secret}") private val secret: String,
+    @Value("\${jwt.accessTokenExpiration}") private val accessTokenExpirationMs: Long,
+    @Value("\${jwt.refreshTokenExpiration}") private val refreshTokenExpirationMs: Long,
+) {
+
+    private val secretKey by lazy {
+        val decodeKey = Base64.getDecoder().decode(secret)
+        Keys.hmacShaKeyFor(decodeKey)
+    }
+
+    fun generateToken(userName: String, tokenType: JwtTokenType): String {
+        val now = Date()
+        val expiredTime = if (tokenType == JwtTokenType.ACCESS) {
+            Date(now.time + accessTokenExpirationMs)
+        } else {
+            Date(now.time + refreshTokenExpirationMs)
+        }
+
+        return Jwts.builder()
+            .subject(userName) // 목적? 인데,,, 보통 userName을 쓰나
+            .issuer("pita")
+            .issuedAt(now)
+            .notBefore(now)
+            .expiration(expiredTime)
+            .signWith(secretKey)
+            .compact()
+    }
+
+    fun validateToken(token: String): Boolean {
+        return runCatching {
+            Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .payload
+            true
+        }.getOrDefault(false)
+    }
+}
