830c52a233169a1f73f1d01b010e7f50bc27b761
Ahn-seokjoo <tjrwn0716@naver.com>
2025-03-29 23:41:33 +0900

    [#16] 필터 등록

--- src/main/kotlin/com/seokjoo/todo/presentation/configuration/TodoConfiguration.kt
+++ src/main/kotlin/com/seokjoo/todo/presentation/configuration/TodoConfiguration.kt
@@ -1,14 +1,27 @@
 package com.seokjoo.todo.presentation.configuration
 
+import com.seokjoo.todo.common.common.jwt.JwtAuthFilter
+import com.seokjoo.todo.common.common.jwt.JwtProvider
+import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
-class TodoConfiguration {
+class TodoConfiguration(
+    private val jwtProvider: JwtProvider,
+) {
     @Bean
     fun passwordEncoder(): PasswordEncoder {
         return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    fun jwtAuthFilter(): FilterRegistrationBean<JwtAuthFilter> {
+        return FilterRegistrationBean(JwtAuthFilter(jwtProvider)).apply {
+            addUrlPatterns("/*")
+            order = 1
+        }
     }
 }
