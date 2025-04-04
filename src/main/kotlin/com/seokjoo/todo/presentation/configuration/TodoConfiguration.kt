12d70178903247a1a3ac70c01f67ae43d73cc225
Ahn-seokjoo <tjrwn0716@naver.com>
2025-04-05 01:33:21 +0900

    [#17] redis 세팅

--- src/main/kotlin/com/seokjoo/todo/presentation/configuration/TodoConfiguration.kt
+++ src/main/kotlin/com/seokjoo/todo/presentation/configuration/TodoConfiguration.kt
@@ -4,10 +4,17 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.seokjoo.todo.common.common.jwt.JwtAuthFilter
 import com.seokjoo.todo.common.common.jwt.JwtProvider
 import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.cache.CacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.cache.RedisCacheManager
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializationContext
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import java.time.Duration
 
 @Configuration
 class TodoConfiguration(
@@ -25,5 +32,21 @@ class TodoConfiguration(
             addUrlPatterns("/*")
             order = 1
         }
+    }
+
+    @Bean
+    fun redisCacheManager(connectionFactory: RedisConnectionFactory): CacheManager {
+        val config = RedisCacheConfiguration.defaultCacheConfig()
+            .disableCachingNullValues()
+            .serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(
+                    GenericJackson2JsonRedisSerializer()
+                )
+            )
+            .entryTtl(Duration.ofMinutes(1)) // 임의의 1분
+
+        return RedisCacheManager.builder(connectionFactory)
+            .cacheDefaults(config)
+            .build()
     }
 }
