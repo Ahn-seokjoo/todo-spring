31c24512e3a08cb616552a354429394d3c935fe7
Ahn-seokjoo <tjrwn0716@naver.com>
2025-04-05 16:30:12 +0900

    [#17] code clean up

--- src/test/kotlin/com/seokjoo/todo/domain/service/todo/TodoServiceSpringBootTest.kt
+++ src/test/kotlin/com/seokjoo/todo/domain/service/todo/TodoServiceSpringBootTest.kt
@@ -11,11 +11,7 @@ import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.data.redis.connection.RedisConnectionFactory
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
-import org.springframework.data.redis.serializer.StringRedisSerializer
 
 @IntegrationTest
 class TodoServiceSpringBootTest @Autowired constructor(
