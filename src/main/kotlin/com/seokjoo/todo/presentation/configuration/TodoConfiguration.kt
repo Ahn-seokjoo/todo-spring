844570cb3b40591c2875fb7f09714159fc1eabec
Ahn-seokjoo <tjrwn0716@naver.com>
2025-04-05 16:08:05 +0900

    [#17] 캐싱 config serializer 수정 redis template bean 등록

--- src/main/kotlin/com/seokjoo/todo/presentation/configuration/TodoConfiguration.kt
+++ src/main/kotlin/com/seokjoo/todo/presentation/configuration/TodoConfiguration.kt
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.seokjoo.todo.common.common.jwt.JwtAuthFilter
 import com.seokjoo.todo.common.common.jwt.JwtProvider
 import com.seokjoo.todo.domain.service.todo.TodoPageServiceResponseDTO
+import com.seokjoo.todo.domain.service.todo.TodoServiceResponseDTO
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.cache.CacheManager
 import org.springframework.context.annotation.Bean
@@ -12,8 +14,12 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.cache.RedisCacheConfiguration
 import org.springframework.data.redis.cache.RedisCacheManager
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.RedisSerializationContext
+import org.springframework.data.redis.serializer.StringRedisSerializer
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import java.time.Duration
@@ -39,15 +45,38 @@ class TodoConfiguration(
     @Bean
     fun redisCacheManager(connectionFactory: RedisConnectionFactory): CacheManager {
         val objectMapper = jacksonObjectMapper()
-        val serializer = Jackson2JsonRedisSerializer(objectMapper, TodoPageServiceResponseDTO::class.java)
+        val pagedTodoSerializer = Jackson2JsonRedisSerializer(objectMapper, TodoPageServiceResponseDTO::class.java)
+        val todoSerializer = Jackson2JsonRedisSerializer(objectMapper, TodoServiceResponseDTO::class.java)
 
-        val config = RedisCacheConfiguration.defaultCacheConfig()
+        val defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
             .disableCachingNullValues()
-            .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer))
             .entryTtl(Duration.ofMinutes(1))
 
+        val pagedTodoConfig = defaultConfig.serializeValuesWith(
+            RedisSerializationContext.SerializationPair.fromSerializer(pagedTodoSerializer)
+        )
+        val todoConfig = defaultConfig.serializeValuesWith(
+            RedisSerializationContext.SerializationPair.fromSerializer(todoSerializer)
+        )
+
         return RedisCacheManager.builder(connectionFactory)
-            .cacheDefaults(config)
+            .cacheDefaults(defaultConfig)
+            .withInitialCacheConfigurations(mapOf("todos" to pagedTodoConfig, "todo" to todoConfig))
             .build()
+    }
+
+    @Bean
+    fun redisConnectionFactory(@Value("\${spring.data.redis.port:6379}") port: Int): RedisConnectionFactory {
+        val redisHost = "localhost"
+        return LettuceConnectionFactory(redisHost, port)
+    }
+
+    @Bean
+    fun redisTemplate(redisConnectionFactory: RedisConnectionFactory): RedisTemplate<String, Any> {
+        return RedisTemplate<String, Any>().apply {
+            connectionFactory = redisConnectionFactory
+            keySerializer = StringRedisSerializer()
+            valueSerializer = GenericJackson2JsonRedisSerializer()
+        }
     }
 }
