1e0b4ccca15b7e8c5e6c709b01c31bf76a20bdf2
Ahn-seokjoo <tjrwn0716@naver.com>
2025-04-06 03:27:20 +0900

    [refactoring] e2e 테스트 리팩토링

--- src/test/kotlin/com/seokjoo/todo/TodoE2ETest.kt
+++ src/test/kotlin/com/seokjoo/todo/TodoE2ETest.kt
@@ -3,14 +3,21 @@ package com.seokjoo.todo
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.seokjoo.todo.domain.service.auth.TodoAuthService
+import com.seokjoo.todo.domain.service.todo.TodoService
+import com.seokjoo.todo.domain.service.todo.TodoServiceRequestDTO
+import com.seokjoo.todo.domain.service.todo.TodoServiceResponseDTO
 import com.seokjoo.todo.presentation.category.dto.CategoryDTO
 import com.seokjoo.todo.presentation.todo.dto.request.TodoRequest
 import com.seokjoo.todo.presentation.todo.dto.response.TodoPageResponse
 import com.seokjoo.todo.presentation.todo.dto.response.TodoResponse
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInfo
+import org.junit.jupiter.api.TestInstance
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.server.LocalServerPort
@@ -21,21 +28,19 @@ import org.springframework.http.HttpMethod
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
-import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.exchange
 import java.net.URI
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 class TodoE2ETest @Autowired constructor(
     private val redisTemplate: RedisTemplate<String, Any>,
     private val objectMapper: ObjectMapper,
-    private val jdbcTemplate: JdbcTemplate,
     private val loginService: TodoAuthService,
+    private val todoService: TodoService,
 ) {
 
     @LocalServerPort
@@ -44,46 +49,7 @@ class TodoE2ETest @Autowired constructor(
     private val restTemplate: RestTemplate = RestTemplate()
 
     private lateinit var header: HttpHeaders
-
-    /**
-     * post로 생성시에 1부터 생성해서 2로 세팅함
-     */
-    @BeforeEach
-    fun beforeEach() {
-        insertInitialData()
-        addHeader()
-    }
-
-    @AfterEach
-    fun cleanupRedis() {
-        val todos = redisTemplate.keys("todos:*")
-        val todo = redisTemplate.keys("todo:*")
-        redisTemplate.delete(todos)
-        redisTemplate.delete(todo)
-    }
-
-    private fun insertInitialData() {
-        val currentTime = LocalDateTime.now()
-        val formattedTime = currentTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
-        val sql = "INSERT INTO todo (todo_id, is_done, todo, created_at, updated_at) VALUES (2, false, 'spring', ?, ?)"
-        jdbcTemplate.update(sql, formattedTime, formattedTime)
-    }
-
-    private fun addHeader() {
-        loginService.signUp("pita", "pita")
-        val result = loginService.login("pita", "pita")
-        header = HttpHeaders().apply {
-            set("Authorization", "Bearer ${result.refreshToken}")
-            contentType = MediaType.APPLICATION_JSON
-        }
-    }
-
-    @AfterEach
-    fun afterEach() {
-        jdbcTemplate.execute("DELETE FROM todo_category WHERE todo_id = 2")
-        jdbcTemplate.execute("DELETE FROM todo WHERE todo_id = 2")
-        loginService.delete("pita", "pita")
-    }
+    private lateinit var todoResponse: TodoServiceResponseDTO
 
     @Test
     fun `GET All todo e2e 테스트`() {
@@ -91,7 +57,7 @@ class TodoE2ETest @Autowired constructor(
         val expected = TodoPageResponse(
             isLast = true,
             todoList = listOf(
-                TodoResponse(id = 2L, todo = "spring", isDone = false, categories = emptyList())
+                TodoResponse(id = 1L, todo = "spring", isDone = false, categories = emptyList())
             )
         )
 
@@ -112,7 +78,7 @@ class TodoE2ETest @Autowired constructor(
 
     @Test
     fun `GET Todo by id e2e 테스트`() {
-        val url = "http://localhost:$port/api/v1/todos/2"
+        val url = "http://localhost:$port/api/v1/todos/${todoResponse.id}"
         val expected = TodoResponse(id = 10L, todo = "spring", isDone = false, categories = emptyList())
 
         val response: ResponseEntity<String> = restTemplate.exchange(
@@ -133,7 +99,8 @@ class TodoE2ETest @Autowired constructor(
     @Test
     fun `POST Todo create e2e 테스트`() {
         val url = "http://localhost:$port/api/v1/todos"
-        val expected = TodoResponse(id = 1L, todo = "Android", isDone = true, categories = listOf("drama"))
+        val expectedId = todoResponse.id + 1
+        val expected = TodoResponse(id = expectedId, todo = "Android", isDone = true, categories = listOf("drama"))
         val request = TodoRequest(todo = "Android", isDone = true, categories = listOf(CategoryDTO("drama")))
 
         val response: ResponseEntity<String> = restTemplate.exchange(
@@ -145,7 +112,7 @@ class TodoE2ETest @Autowired constructor(
         val result = objectMapper.readValue<TodoResponse>(response.body.orEmpty())
 
         assertThat(response.statusCode.value()).isEqualTo(201)
-        assertThat(response.headers.location).isEqualTo(URI.create("/todos/1"))
+        assertThat(response.headers.location).isEqualTo(URI.create("/todos/${expectedId}"))
         assertThat(result)
             .usingRecursiveComparison()
             .ignoringFields("id")
@@ -154,7 +121,7 @@ class TodoE2ETest @Autowired constructor(
 
     @Test
     fun `DELETE Todo delete e2e 테스트`() {
-        val url = "http://localhost:$port/api/v1/todos/1"
+        val url = "http://localhost:$port/api/v1/todos/${todoResponse.id}"
 
         val response: ResponseEntity<String> = restTemplate.exchange(
             url = url,
@@ -171,8 +138,8 @@ class TodoE2ETest @Autowired constructor(
         val restTemplate = RestTemplate().apply {
             requestFactory = HttpComponentsClientHttpRequestFactory()
         }
-        val url = "http://localhost:$port/api/v1/todos/2"
-        val expected = TodoResponse(id = 2L, todo = "node", isDone = true, categories = listOf("drama"))
+        val url = "http://localhost:$port/api/v1/todos/${todoResponse.id}"
+        val expected = TodoResponse(id = 1L, todo = "node", isDone = true, categories = listOf("drama"))
         val request = TodoRequest(todo = "node", isDone = true, categories = listOf(CategoryDTO("drama")))
 
         val responseEntity: ResponseEntity<String> =
@@ -184,5 +151,43 @@ class TodoE2ETest @Autowired constructor(
             .usingRecursiveComparison()
             .ignoringFields("id")
             .isEqualTo(expected)
+    }
+
+    @BeforeEach
+    fun beforeEach() {
+        todoResponse = todoService.createTodo(
+            TodoServiceRequestDTO(
+                todo = "spring",
+            )
+        )
+    }
+
+    @AfterEach
+    fun afterEach(testInfo: TestInfo) {
+        with(redisTemplate) {
+            delete(keys("todos:*"))
+            delete(keys("todo:*"))
+        }
+        if (testInfo.displayName.contains("DELETE Todo delete e2e 테스트")) return
+        todoService.deleteTodo(todoResponse.id)
+    }
+
+    @BeforeAll
+    fun beforeAll() {
+        loginService.signUp("pita", "pita")
+        createHeader()
+    }
+
+    private fun createHeader() {
+        val refreshToken = loginService.login("pita", "pita").refreshToken
+        header = HttpHeaders().apply {
+            set("Authorization", "Bearer $refreshToken")
+            contentType = MediaType.APPLICATION_JSON
+        }
+    }
+
+    @AfterAll
+    fun afterAll() {
+        loginService.delete("pita", "pita")
     }
 }
