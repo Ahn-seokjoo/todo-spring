4da0702a5f7da8ac6ecec1cc7d8769b73aafba35
Ahn-seokjoo <tjrwn0716@naver.com>
2025-04-06 01:30:10 +0900

    [#17] 특정 DTO가 아닌 모든 class에 대해서 직/역직렬화 되더록 수정

--- src/main/kotlin/com/seokjoo/todo/domain/service/todo/TodoServiceResponseDTO.kt
+++ src/main/kotlin/com/seokjoo/todo/domain/service/todo/TodoServiceResponseDTO.kt
@@ -1,14 +1,16 @@
 package com.seokjoo.todo.domain.service.todo
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonTypeInfo
 import com.seokjoo.todo.domain.entity.todo.Todo
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
 data class TodoServiceResponseDTO(
-    val id: Long,
-    val todo: String,
+    val id: Long = 0L,
+    val todo: String = "",
     @JsonProperty("isDone") // is가 붙으면 is를 빼버리고 내부에 저장해버림;;
-    val isDone: Boolean,
-    val categories: List<String>,
+    val isDone: Boolean = false,
+    val categories: List<String> = emptyList(),
 ) {
     companion object {
         fun from(todo: Todo) = TodoServiceResponseDTO(
--- src/main/kotlin/com/seokjoo/todo/presentation/configuration/TodoConfiguration.kt
+++ src/main/kotlin/com/seokjoo/todo/presentation/configuration/TodoConfiguration.kt
@@ -1,11 +1,13 @@
 package com.seokjoo.todo.presentation.configuration
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.seokjoo.todo.common.common.jwt.JwtAuthFilter
 import com.seokjoo.todo.common.common.jwt.JwtProvider
-import com.seokjoo.todo.domain.service.todo.TodoPageServiceResponseDTO
-import com.seokjoo.todo.domain.service.todo.TodoServiceResponseDTO
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.cache.CacheManager
@@ -17,7 +19,6 @@ import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.RedisSerializationContext
 import org.springframework.data.redis.serializer.StringRedisSerializer
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
@@ -45,23 +46,28 @@ class TodoConfiguration(
     @Bean
     fun redisCacheManager(connectionFactory: RedisConnectionFactory): CacheManager {
         val objectMapper = jacksonObjectMapper()
-        val pagedTodoSerializer = Jackson2JsonRedisSerializer(objectMapper, TodoPageServiceResponseDTO::class.java)
-        val todoSerializer = Jackson2JsonRedisSerializer(objectMapper, TodoServiceResponseDTO::class.java)
+            .registerModule(JavaTimeModule())
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .apply {
+                activateDefaultTyping(
+                    LaissezFaireSubTypeValidator.instance,
+                    ObjectMapper.DefaultTyping.NON_FINAL,
+                    JsonTypeInfo.As.PROPERTY
+                )
+            }
 
         val defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
             .disableCachingNullValues()
+            .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer()))
+            .serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(
+                    GenericJackson2JsonRedisSerializer(objectMapper)
+                )
+            )
             .entryTtl(Duration.ofMinutes(1))
-
-        val pagedTodoConfig = defaultConfig.serializeValuesWith(
-            RedisSerializationContext.SerializationPair.fromSerializer(pagedTodoSerializer)
-        )
-        val todoConfig = defaultConfig.serializeValuesWith(
-            RedisSerializationContext.SerializationPair.fromSerializer(todoSerializer)
-        )
 
         return RedisCacheManager.builder(connectionFactory)
             .cacheDefaults(defaultConfig)
-            .withInitialCacheConfigurations(mapOf("todos" to pagedTodoConfig, "todo" to todoConfig))
             .build()
     }
 
