4bfe2b807584f3d61bca765c010bc8cf1e2849e5
Ahn-seokjoo <tjrwn0716@naver.com>
2025-04-30 15:47:53 +0900

    [#54] 유저 엔티티 리팩토링 및 잔액 추가

--- src/main/kotlin/com/seokjoo/todo/domain/entity/todouser/User.kt
+++ src/main/kotlin/com/seokjoo/todo/domain/entity/todouser/User.kt
@@ -6,14 +6,20 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 
-@Entity(name = "todo_user")
+@Entity
+@Table(name = "todo_user")
 class User(
+    @Column(name = "user_id", nullable = false)
     val userId: String,
+    @Column(nullable = false)
     val password: String,
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "todo_user_id")
     val id: Long = 0L,
+
+    var balance: Long = 0L,
 ) : BaseEntity()
