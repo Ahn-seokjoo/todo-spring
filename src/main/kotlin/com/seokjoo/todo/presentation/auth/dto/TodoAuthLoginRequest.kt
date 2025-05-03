c3aadeccd1ee339ca06a5522bf3e7f1fe2078077
Ahn-seokjoo <tjrwn0716@naver.com>
2025-05-03 23:45:00 +0900

    [#54] 리부 반영 - user_id로 수정 !

--- src/main/kotlin/com/seokjoo/todo/presentation/auth/dto/TodoAuthLoginRequest.kt
+++ src/main/kotlin/com/seokjoo/todo/presentation/auth/dto/TodoAuthLoginRequest.kt
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank
 
 data class TodoAuthLoginRequest(
     @field:NotBlank(message = "id는 필수 값입니다.")
-    val userId: String,
+    val user_id: String,
     @field:NotBlank(message = "password는 필수 값입니다.")
     val password: String,
 )
