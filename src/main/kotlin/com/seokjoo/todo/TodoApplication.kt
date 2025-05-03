4cfee05c66370a0217a94ff50f4fb82d466dcacb
Ahn-SeokJoo <67602108+Ahn-seokjoo@users.noreply.github.com>
2025-05-04 01:06:56 +0900

    [#60] created at, updated at 수정 (#62)

--- src/main/kotlin/com/seokjoo/todo/TodoApplication.kt
+++ src/main/kotlin/com/seokjoo/todo/TodoApplication.kt
@@ -3,9 +3,11 @@ package com.seokjoo.todo
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
 @EnableCaching
+@EnableJpaAuditing
 class TodoApplication
 
 fun main(args: Array<String>) {
