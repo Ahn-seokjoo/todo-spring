0df55b333e11629bb7f9d864ba604e2f04057ef0
Ahn-seokjoo <tjrwn0716@naver.com>
2025-05-03 23:48:30 +0900

    [#54] 리부 반영 - 재수정~

--- src/main/kotlin/com/seokjoo/todo/presentation/auth/dto/TodoAuthLoginRequest.kt
+++ src/main/kotlin/com/seokjoo/todo/presentation/auth/dto/TodoAuthLoginRequest.kt
@@ -1,10 +1,12 @@
 package com.seokjoo.todo.presentation.auth.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.validation.constraints.NotBlank
 
 data class TodoAuthLoginRequest(
     @field:NotBlank(message = "id는 필수 값입니다.")
-    val user_id: String,
+    @JsonProperty("user_id")
+    val userId: String,
     @field:NotBlank(message = "password는 필수 값입니다.")
     val password: String,
 )
