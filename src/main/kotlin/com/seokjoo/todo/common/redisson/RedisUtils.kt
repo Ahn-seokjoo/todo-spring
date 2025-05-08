483b0403ac29e1e6594364c59275ca5fb932fcae
Ahn-seokjoo <tjrwn0716@naver.com>
2025-05-09 00:31:49 +0900

    [#47] lock 주석 및 catch 추가

--- src/main/kotlin/com/seokjoo/todo/common/redisson/RedisUtils.kt
+++ src/main/kotlin/com/seokjoo/todo/common/redisson/RedisUtils.kt
@@ -11,7 +11,11 @@ class RedisUtils(
     fun tryLock(key: String, block: () -> Unit) {
         val lock = redisson.getLock(key)
         try {
+            // 5초간 락 시도, 3초간 락을 유지
             if (lock.tryLock(5, 3, TimeUnit.SECONDS)) block.invoke()
+        } catch (e: IllegalStateException) {
+            // do nothing
+            // 왜냐하면, lock걸고 추가하는 카테고리의 경우, 1개만 들어가면 되기 때문~
         } finally {
             lock.unlock()
         }
