822e8a2f985eeb63ab490029959edede9c853bad
Ahn-seokjoo <tjrwn0716@naver.com>
2026-03-15 17:17:01 +0900

    [#86] user_id 응답 변경

--- src/main/kotlin/com/seokjoo/todo/presentation/charge/dto/TodoChargeResponse.kt
+++ src/main/kotlin/com/seokjoo/todo/presentation/charge/dto/TodoChargeResponse.kt
@@ -1,6 +1,9 @@
 package com.seokjoo.todo.presentation.charge.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class TodoChargeResponse(
+    @field:JsonProperty("user_id")
     val userId: String,
     val amount: Long,
 )
