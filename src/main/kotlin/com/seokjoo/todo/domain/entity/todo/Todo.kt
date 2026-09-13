2f0625609e554550a69e0bd1a613517f96933b53
Ahn-seokjoo <tjrwn0716@naver.com>
2026-09-14 01:43:09 +0900

    [Refactor] Todo 에도 낙관락 추가

--- src/main/kotlin/com/seokjoo/todo/domain/entity/todo/Todo.kt
+++ src/main/kotlin/com/seokjoo/todo/domain/entity/todo/Todo.kt
@@ -15,6 +15,7 @@ import jakarta.persistence.Id
 import jakarta.persistence.ManyToOne
 import jakarta.persistence.OneToMany
 import jakarta.persistence.Table
+import jakarta.persistence.Version
 
 @Entity
 @Table(name = "todo")
@@ -35,6 +36,10 @@ class Todo(
 
     @ManyToOne(fetch = FetchType.LAZY)
     var owner: User,
+
+    @Version
+    @Column(nullable = false)
+    var version: Long = 0L,
 ) : BaseEntity() {
     fun hasCategory(categoryName: String): Boolean {
         return todoCategories.any { it.category?.name == categoryName }
