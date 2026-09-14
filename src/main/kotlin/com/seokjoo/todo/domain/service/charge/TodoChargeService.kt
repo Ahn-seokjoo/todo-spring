bc6afc2fb9ea9f58bb426d3d6c610413f1763a68
Ahn-seokjoo <tjrwn0716@naver.com>
2026-09-15 01:23:13 +0900

    [Refactor] Charge, Balance 분리하여 Retry, Transactional 분리

--- src/main/kotlin/com/seokjoo/todo/domain/service/charge/TodoChargeService.kt
+++ src/main/kotlin/com/seokjoo/todo/domain/service/charge/TodoChargeService.kt
@@ -1,26 +1,22 @@
 package com.seokjoo.todo.domain.service.charge
 
 import com.seokjoo.todo.domain.entity.todouser.User
-import com.seokjoo.todo.domain.service.auth.TodoAuthService
+import com.seokjoo.todo.domain.service.balance.TodoBalanceService
 import org.springframework.orm.ObjectOptimisticLockingFailureException
 import org.springframework.retry.annotation.Backoff
 import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 
 @Service
 class TodoChargeService(
-    private val todoAuthService: TodoAuthService,
+    private val todoBalanceService: TodoBalanceService,
 ) {
     @Retryable(
         retryFor = [ObjectOptimisticLockingFailureException::class],
         maxAttempts = 5,
         backoff = Backoff(delay = 100L, maxDelay = 3000L, random = true, multiplier = 2.0)
     )
-    @Transactional
     fun charge(amount: Long, userId: String): User {
-        val owner = todoAuthService.findUserByUserId(userId)
-        owner.increaseBalance(amount)
-        return owner
+        return todoBalanceService.increaseBalance(amount, userId)
     }
 }
