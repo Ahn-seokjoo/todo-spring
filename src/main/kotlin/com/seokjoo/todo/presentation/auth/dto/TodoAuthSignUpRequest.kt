b77793fc2da25d365d779acc83c3b5aaa150d65b
Ahn-seokjoo <tjrwn0716@naver.com>
2026-09-20 19:40:11 +0900

    [Refactor] login, signup request 분리

--- src/main/kotlin/com/seokjoo/todo/presentation/auth/dto/TodoAuthSignUpRequest.kt
+++ src/main/kotlin/com/seokjoo/todo/presentation/auth/dto/TodoAuthSignUpRequest.kt
@@ -3,10 +3,12 @@ package com.seokjoo.todo.presentation.auth.dto
 import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.validation.constraints.NotBlank
 
-data class TodoAuthLoginRequest(
+data class TodoAuthSignUpRequest(
     @field:NotBlank(message = "id는 필수 값입니다.")
     @field:JsonProperty("user_id")
     val userId: String,
     @field:NotBlank(message = "password는 필수 값입니다.")
     val password: String,
+    @field:NotBlank(message = "todo 구매시에 확정받을 이메일을 입력하세요")
+    val email: String,
 )
